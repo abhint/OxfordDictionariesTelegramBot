fa689b48e512f9923324a25aafdf1ee588720933
AbhijithNT <abhijithnt64@gmail.com>
2021-10-12 20:59:16 +0530

    API_HASH

--- bot/oxforddictionaries.py
+++ bot/oxforddictionaries.py
@@ -8,6 +8,7 @@ class OD(Client):
     def __init__(self,):
         super().__init__("session_name",
                          api_id=os.environ.get("API_ID"),
+                         api_hash=os.environ.get("API_HASH"),
                          bot_token=os.environ.get("BOT_TOKEN"),
                          )
 
