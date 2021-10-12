8dd0fcde9c7a5c4ef2a5825f89138111ea2b4fda
AbhijithNT <abhijithnt64@gmail.com>
2021-10-12 20:47:09 +0530

    Client

---
+++ bot/__main__.py
@@ -0,0 +1,18 @@
+from pyrogram import Client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class OD(Client):
+    def __init__(self,):
+        super().__init__("session_name",
+                         bot_token=os.environ.get["BOT_TOKEN"],
+                         )
+
+    async def start(self):
+        await super().start()
+        print(f"{self.get_chat}")
+
+    async def stop(self, *args):
+        await super().stop()
