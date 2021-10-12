20f0da2fdc35f0138e5d78fab44ad0d653aff37c
AbhijithNT <abhijithnt64@gmail.com>
2021-10-12 20:49:35 +0530

    run me

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,18 +1,5 @@
-from pyrogram import Client
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from .oxforddictionaries import OD
 
 
-class OD(Client):
-    def __init__(self,):
-        super().__init__("session_name",
-                         bot_token=os.environ.get["BOT_TOKEN"],
-                         )
-
-    async def start(self):
-        await super().start()
-        print(f"{self.get_chat}")
-
-    async def stop(self, *args):
-        await super().stop()
+if __name__ == "__main__":
+    OD().run()
