c0cbd740e7b695bca2c08c6be610f3b8960e9224
AbhijithNT <abhijithnt64@gmail.com>
2021-10-18 23:38:31 +0530

    Help text

--- bot/plugins/cmd.py
+++ bot/plugins/cmd.py
@@ -2,8 +2,14 @@
 
 from ..oxforddictionaries import OD
 from pyrogram import filters
-from bot import START_TEXT
+from bot import START_TEXT, HELP_TEXT
+
 
 @OD.on_message(filters.command(["start"]))
 async def startMsg(_, msg):
     await msg.reply(START_TEXT)
+
+
+@OD.on_deleted_messages(filters.command(['help', 'h']))
+async def onHelp(_, msg):
+    msg.reply(HELP_TEXT)
