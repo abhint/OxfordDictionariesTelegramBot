2c31e931dfc3b791e8a5e70af26820f0dc9c2b96
AbhijithNT <abhijithnt64@gmail.com>
2021-10-18 22:57:09 +0530

    import START_TEXT

--- bot/plugins/cmd.py
+++ bot/plugins/cmd.py
@@ -1,7 +1,7 @@
 from ..oxforddictionaries import OD
 from pyrogram import filters
-
+from bot import START_TEXT
 
 @OD.on_message(filters.command(["start"]))
 async def startMsg(_, msg):
-    await msg.reply("Hello, World!")
+    await msg.reply(START_TEXT)
