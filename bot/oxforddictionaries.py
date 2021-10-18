a4a08c9cf6fcd4b9c394410e44e3052f3a911a40
AbhijithNT <abhijithnt64@gmail.com>
2021-10-18 22:55:10 +0530

    import

--- bot/oxforddictionaries.py
+++ bot/oxforddictionaries.py
@@ -1,15 +1,16 @@
 from pyrogram import Client
 import os
-from dotenv import load_dotenv
-load_dotenv()
+from bot import (API_ID,
+                 API_HASH,
+                 BOT_TOKEN)
 
 
 class OD(Client):
     def __init__(self,):
-        super().__init__("session_name",
-                         api_id=os.environ.get("API_ID"),
-                         api_hash=os.environ.get("API_HASH"),
-                         bot_token=os.environ.get("BOT_TOKEN"),
+        super().__init__('session_name',
+                         api_id=API_ID,
+                         api_hash=API_HASH,
+                         bot_token=BOT_TOKEN,
                          plugins={
                              'root': 'bot/plugins'
                          },
